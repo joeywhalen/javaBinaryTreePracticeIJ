0e6b3f52ad62f636b79ae80fac0825f0872038d5
Joey Whalen <jtktwhalen@gmail.com>
2021-02-09 16:55:21 -0500

    update BT

--- src/javaBinaryTreePracticeIJ/BinaryTree.java
+++ src/javaBinaryTreePracticeIJ/BinaryTree.java
@@ -150,6 +150,22 @@ public class BinaryTree {
     }
   }
 
+  public void traversePreOrderWithoutRecursion() {
+    Stack<Node> stack = new Stack<Node>();
+    Node current = root;
+    stack.push(root);
+    while (! stack.isEmpty()) {
+      current = stack.pop();
+      visit(current.value);
+
+      if (current.right != null)
+        stack.push(current.right);
+
+      if (current.left != null)
+        stack.push(current.left);
+    }
+  }
+
   
 
 }
