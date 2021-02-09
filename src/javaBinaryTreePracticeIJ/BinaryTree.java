a435b61244401c18bffc9b1cd3bf376b67d3824c
Joey Whalen <jtktwhalen@gmail.com>
2021-02-09 17:12:09 -0500

    update BT program complete

--- src/javaBinaryTreePracticeIJ/BinaryTree.java
+++ src/javaBinaryTreePracticeIJ/BinaryTree.java
@@ -166,6 +166,46 @@ public class BinaryTree {
     }
   }
 
-  
+  public void traversePostOrderWithoutRecursion() {
+    Stack<Node> stack = new Stack<Node>();
+    Node prev = root;
+    Node current = root;
+    stack.push(root);
+
+    while (!stack.isEmpty()) {
+      current = stack.peek();
+      boolean hasChild = (current.left != null || current.right != null);
+      boolean isPrevLastChild = (prev == current.right || (prev == current.left && current.right == null));
+
+      if (!hasChild || isPrevLastChild) {
+        current = stack.pop();
+        visit(current.value);
+        prev = current;
+      } else {
+        if (current.right != null) {
+          stack.push(current.right);
+        }
+        if (current.left != null) {
+          stack.push(current.left);
+        }
+      }
+    }
+  }
+
+  private void visit(int value) {
+    System.out.print(" " + value);
+  }
+
+  class Node {
+    int value;
+    Node left;
+    Node right;
+
+    Node(int value) {
+      this.value = value;
+      right = null;
+      left = null;
+    }
+  }
 
 }
